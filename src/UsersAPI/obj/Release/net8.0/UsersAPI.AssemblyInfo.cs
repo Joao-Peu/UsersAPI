2dd268a172b735babe1ecf0353e0218af177a34a
GabrielVeridico <gabrielvmarra1@hotmail.com>
2026-01-12 20:38:22 -0300

    chore: gitignore e sln

--- src/UsersAPI/obj/Release/net8.0/UsersAPI.AssemblyInfo.cs
+++ src/UsersAPI/obj/Release/net8.0/UsersAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6de8244369152b0fe10a0501fbf53c75c98d810")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
